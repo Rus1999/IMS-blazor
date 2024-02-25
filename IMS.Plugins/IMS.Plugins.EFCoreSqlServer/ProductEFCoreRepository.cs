203c75f6f3c900af01f581cde92a44a67ea2ff6d
Rus@home <punyarit07@gmail.com>
2024-02-25 18:02:38 +0700

    63

--- IMS.Plugins/IMS.Plugins.EFCoreSqlServer/ProductEFCoreRepository.cs
+++ IMS.Plugins/IMS.Plugins.EFCoreSqlServer/ProductEFCoreRepository.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IMS.Plugins.InMemory
+namespace IMS.Plugins.EFCoreSqlServer
 {
     public class ProductEFCoreRepository : IProductRepository
     {
