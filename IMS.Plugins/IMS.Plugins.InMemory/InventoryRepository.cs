04e507b609aa2c82605692b575dad470311257a9
Rus@home <punyarit07@gmail.com>
2024-02-25 16:17:05 +0700

    58

--- IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
+++ IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
@@ -14,8 +14,8 @@ namespace IMS.Plugins.InMemory
             {
                 new Inventory() { InventoryId = 1, InventoryName = "Bike Seat", Quantity = 10, Price = 2},
                 new Inventory() { InventoryId = 2, InventoryName = "Bike Body", Quantity = 10, Price = 15 },
-                new Inventory() { InventoryId = 3, InventoryName = "Bike Wheels", Quantity = 20, Price = 8 },
-                new Inventory() { InventoryId = 4, InventoryName = "Bike Pedels", Quantity = 20, Price = 1 },
+                new Inventory() { InventoryId = 3, InventoryName = "Bike Wheel", Quantity = 20, Price = 8 },
+                new Inventory() { InventoryId = 4, InventoryName = "Bike Pedal", Quantity = 20, Price = 1 },
             };
         }
 
